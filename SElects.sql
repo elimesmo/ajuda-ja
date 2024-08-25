f5281d8928ee1b000b09475016960ca8efab4009
IFGabriel07 <159431940+IFGabriel07@users.noreply.github.com>
2024-08-24 21:14:59 -0300

    Update SElects.sql

--- SElects.sql
+++ SElects.sql
@@ -1,3 +1,4 @@
+	
 # Saber os servidores que estão nesse horario disponivel 
 select * from servidor where Horario_Disponivel = "6:00";
 
@@ -8,7 +9,7 @@ select * from servidorPagamento where fk_pagamento_cod = "1";
 select * from profissao where nome = "professor";
 
 # ele vai contar quantos cliente esse servidor TONHO@GAMIL.COM tem
-select COUNT(fk_cliente_cpf) from clienteServidor where fk_servidor_cpf = "313.114.316-90";
+select COUNT(fk_cliente_cpf) from clienteServidor where fk_servidor_email = "TONHO@GMAIL.COM";
 
 # Vai pegar o servidor que recebe o valor menor que 200.00 
 select * from servidor where valor <= 200.00;
@@ -22,7 +23,9 @@ select * from servidor ORDER BY valor desc;
 # vai ordenar os sevidores em ordem alfabetica
 SELECT * FROM servidor ORDER BY nome;
 
-# vai pegar os valores até 200
-select * from servidor where valor limit 200;
-
-select 
+#Top 3 dos Servidores que prestaram mais serviços 
+SELECT fk_servidor_email AS TOP3_Servidores, COUNT(*) AS Qntd FROM servidorPagamento 
+GROUP BY fk_servidor_email ORDER BY Qntd DESC LIMIT 3;
+	
+#Essa tabela mostra o total de serviços realizados
+SELECT COUNT(*) AS Servicos_Totais FROM clienteServidor;
